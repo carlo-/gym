Fix reward when arm rotation control is disabled (moving hand env.)

playground/robotics/shadow_hand.py
@@ -1,13 +1,47 @@
 import itertools as it
+from threading import Thread
 
 import numpy as np
 import gym
 from gym.utils.mjviewer import add_selection_logger
 
+from playground.utils import wait_for_key
+
+selected_action = np.zeros(4)
+
+
+def action_from_key():
+    key = wait_for_key()
+    if key == 'A':
+        return np.r_[-1, 0, 0, 0, 0, 0, 0]
+    elif key == 'B':
+        return np.r_[1, 0, 0, 0, 0, 0, 0]
+    elif key == 'C':
+        return np.r_[0, 1, 0, 0, 0, 0, 0]
+    elif key == 'D':
+        return np.r_[0, -1, 0, 0, 0, 0, 0]
+    elif key == 'w':
+        return np.r_[0, 0, 1, 0, 0, 0, 0]
+    elif key == 's':
+        return np.r_[0, 0, -1, 0, 0, 0, 0]
+    elif key == 'z':
+        return np.r_[0, 0, 0, 1, 0, 0, 0]
+    elif key == 'x':
+        return np.r_[0, 0, 0, -1, 0, 0, 0]
+    else:
+        return np.zeros(7)
+
+
+def action_thread():
+    global selected_action
+    while True:
+        a = action_from_key().astype(np.float64)
+        selected_action = a
+
 
 def main():
     # env = gym.make('HandPickAndPlaceDense-v0')
-    env = gym.make('MovingHandReachDense-v0', ignore_rotation_ctrl=True)
+    env = gym.make('MovingHandReach-v0', ignore_rotation_ctrl=True, ignore_target_rotation=True)
     env.reset()
 
     env.render()
@@ -15,20 +49,28 @@ def main():
     add_selection_logger(env.unwrapped.viewer, sim)
     print('nconmax:', sim.model.nconmax)
 
+    global selected_action
+    p = Thread(target=action_thread)
+    p.start()
+    selected_action = np.zeros(7)
+
     for i in it.count():
 
         for j in range(6):
 
             env.reset()
-            for val in np.linspace(-1, 1, 60):
+            # for val in np.linspace(-1, 1, 60):
+            while True:
 
                 env.render()
                 action = env.action_space.sample()
-                action[-7:] *= 0.0
-                action[20+j] = val
+                # action[-7:] *= 0.0
+                # action[20+j] = val
+                action[-7:] = selected_action * 0.2
+                selected_action *= 0.0
 
                 rew = env.step(action)[1]
-                # print(rew)
+                print(rew)
 
 
 if __name__ == '__main__':