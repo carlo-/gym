fddfde71c91518ee788dde44a89308c12e86d930
Carlo Rapisarda <carlora@kth.se>
2019-05-16 10:55:55 +0200

    Add FetchPushAgent

--- gym/agents/fetch.py
+++ gym/agents/fetch.py
@@ -62,10 +62,64 @@ class FetchPickAndPlaceAgent(BaseAgent):
             return action
 
 
+class FetchPushAgent(BaseAgent):
+
+    def __init__(self, env, **kwargs):
+        super(FetchPushAgent, self).__init__(env, **kwargs)
+        self._goal = None
+        self._phase = 0
+        self._last_disp = None
+
+    def predict(self, obs, **kwargs):
+
+        goal = obs['desired_goal']
+        if self._goal is None or np.any(goal != self._goal):
+            self._goal = goal.copy()
+            self._phase = 0
+            self._last_disp = None
+
+        grp_pos = obs['observation'][:3]
+        object_pos = obs['observation'][3:6]
+        # object_rel_pos = obs['observation'][6:9]
+
+        disp = self._goal[:2] - object_pos[:2]
+        if self._last_disp is None or np.linalg.norm(disp) > 0.05:
+            self._last_disp = disp.copy()
+
+        disp_dir = self._last_disp / np.linalg.norm(self._last_disp)
+
+        push_goal_pos = object_pos - np.r_[disp_dir, 0.] * 0.06
+
+        if self._phase == 0:
+            push_goal_pos[2] = 0.5
+        elif self._phase == 2:
+            push_goal_pos[:2] += np.clip(disp*1.7, -0.08, 0.08)
+
+        grp_disp = push_goal_pos - grp_pos
+
+        action = np.zeros(4)
+        action[-1] = -1.0
+
+        if np.linalg.norm(disp) < 0.01:
+            return action
+
+        action[:2] = grp_disp[:2] * 8.
+        action[2] = grp_disp[2] * 8.
+
+        if np.linalg.norm(grp_disp) < 0.01:
+            if self._phase == 0:
+                self._phase = 1
+            elif self._phase == 1:
+                self._phase = 2
+
+        return action
+
+
 def _main():
-    env = gym.make('FetchPickAndPlaceDense-v1')
-    env.unwrapped.reward_params = dict(stepped=True)
-    agent = FetchPickAndPlaceAgent(env)
+    env = gym.make('FetchPickAndPlace-v1')
+    # env.unwrapped.reward_params = dict(stepped=True)
+    env.unwrapped.target_in_the_air = False
+    agent = FetchPushAgent(env)
     obs = None
     done = True
     while True:
